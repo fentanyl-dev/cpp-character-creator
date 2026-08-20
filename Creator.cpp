56653b7101d6a35014a37962f9810ef37db85708
Kacper <fentanyl@Kacpers-MacBook-Pro.local>
2026-08-20 16:16:43 +0200

    Refractor character creator into functions

--- Creator.cpp
+++ Creator.cpp
@@ -11,16 +11,125 @@ class Player
             "Mag",
             "Warrior"
         };
+        string characterRace[3] = {
+            "Elf",
+            "Human",
+            "Dwarf"
+        };
 
-        class CharacterAtributes {
+        class CharacterAttributes {
             public:
                 int hp;
                 int mana;
-                int strenght;
+                int strength;
         };
 
-        CharacterAtributes atributes;
+        CharacterAttributes attributes;
+
 };
+
+void setClassAttributes (Player& characterInfo, int choiceOfClass) 
+{
+ if (choiceOfClass == 1)
+    {
+        characterInfo.attributes.hp = 50;
+        characterInfo.attributes.mana = 100;
+        characterInfo.attributes.strength = 20;
+    }
+    else if (choiceOfClass == 2) 
+    {
+        characterInfo.attributes.hp = 80;
+        characterInfo.attributes.mana = 150;
+        characterInfo.attributes.strength = 40;
+    }
+    else if (choiceOfClass == 3)
+    {
+        characterInfo.attributes.hp = 100;
+        characterInfo.attributes.mana = 30;
+        characterInfo.attributes.strength = 70;
+    }
+
+}
+
+void setRaceAttributes (Player& characterInfo, int choiceOfRace)
+{
+    if (choiceOfRace == 1)
+    {
+        characterInfo.attributes.hp += 20;
+        characterInfo.attributes.mana += 10;
+        characterInfo.attributes.strength += 15;
+    }
+
+    else if (choiceOfRace == 2)
+    {
+        characterInfo.attributes.hp += 10;
+        characterInfo.attributes.mana += 30;
+        characterInfo.attributes.strength += 45;
+    }
+    
+    else if (choiceOfRace == 3)
+    {
+        characterInfo.attributes.hp += 30;
+        characterInfo.attributes.mana += 10;
+        characterInfo.attributes.strength += 5;
+    }
+}
+
+int chooseClass()
+{
+    int choiceOfClass;
+    cout << "Wpisz numer odpowiadajacej klasy: ";
+    cin >> choiceOfClass;
+
+    while (choiceOfClass < 1 || choiceOfClass > 3)
+    {
+        cout << "Nieprawidlowy wybor! Wybierz klase od 1 do 3: ";
+        cin >> choiceOfClass;
+    }
+
+    return choiceOfClass;
+}
+
+int chooseRace()
+{
+    int choiceOfRace;
+    cout << "Wpisz numer odpowiedniej rasy: ";
+    cin >> choiceOfRace;
+
+    while (choiceOfRace < 1 || choiceOfRace > 3) 
+    {
+        cout << "Nieprawidlowy wybor rasy! Wybierz rase od 1 do 3!: " << endl;
+        cin >> choiceOfRace;
+
+    }
+    return choiceOfRace;
+}
+
+void saveCharacter (Player& characterInfo, int choiceOfClass, int choiceOfRace)
+{
+    ofstream file("Character.txt");  
+
+    if (file.is_open())
+    {
+        file << "=================" << endl;
+        file << "TWOJA POSTAC: " << endl;
+        file << "Imie: " << characterInfo.characterName << endl;
+        file << "Klasa: " << characterInfo.characterClass[choiceOfClass - 1] << endl;
+        file << "Rasa: " << characterInfo.characterRace[choiceOfRace - 1] << endl;
+        file << "HP: " << characterInfo.attributes.hp << endl;
+        file << "Mana: " << characterInfo.attributes.mana << endl;
+        file << "Strenght: " << characterInfo.attributes.strength << endl;
+        file << "=================";
+
+        file.close();
+
+        cout << "Plik zostal zapisany poprawnie!" << endl;
+    }
+    else 
+    {
+        cout << "Plik nie zostal zapisany ;/" << endl;
+    }
+}
 
 int main() {
     cout << "Witaj w Kreatorze postaci" << endl;
@@ -38,52 +147,28 @@ int main() {
         cout << i + 1 << ". " << characterInfo.characterClass[i] << endl;
     }
 
-    int choiceOfClass;
-    cout << "Wpisz numer odpowiadajacej klasy: ";
-    cin >> choiceOfClass;
+    int choiceOfClass = chooseClass();
 
-    if (choiceOfClass == 1)
-    {
-        characterInfo.atributes.hp = 50;
-        characterInfo.atributes.mana = 100;
-        characterInfo.atributes.strenght = 20;
-    }
-    else if (choiceOfClass == 2) 
-    {
-        characterInfo.atributes.hp = 80;
-        characterInfo.atributes.mana = 150;
-        characterInfo.atributes.strenght = 40;
-    }
-    else if (choiceOfClass == 3)
-    {
-        characterInfo.atributes.hp = 100;
-        characterInfo.atributes.mana = 30;
-        characterInfo.atributes.strenght = 70;
-    }
-
+    setClassAttributes(characterInfo, choiceOfClass);
+    
     cout << "Wybrana Klasa: " << characterInfo.characterClass[choiceOfClass - 1] << endl;
 
-    ofstream file("Character.txt");  
+    //RACE
 
-    if (file.is_open())
+    cout << "Teraz wybierz rase swojej postaci: " << endl;
+
+    for (int i = 0; i < 3; i++)
     {
-        file << "=================" << endl;
-        file << "TWOJA POSTAC: " << endl;
-        file << "Imie: " << characterInfo.characterName << endl;
-        file << "Klasa: " << characterInfo.characterClass[choiceOfClass - 1] << endl;
-        file << "HP: " << characterInfo.atributes.hp << endl;
-        file << "Mana: " << characterInfo.atributes.mana << endl;
-        file << "Strenght: " << characterInfo.atributes.strenght << endl;
-        file << "=================";
-
-        file.close();
-
-        cout << "Plik zostal zapisany poprawnie!" << endl;
+        cout << i + 1 << ". " << characterInfo.characterRace[i] << endl;
     }
-    else 
-    {
-        cout << "Plik nie zostal zapisany ;/" << endl;
-    }
+
+    int choiceOfRace = chooseRace();
+    
+    setRaceAttributes(characterInfo, choiceOfRace);
+
+    //FILE
+    
+    saveCharacter(characterInfo, choiceOfClass, choiceOfRace);
     
     return 0;
 }
